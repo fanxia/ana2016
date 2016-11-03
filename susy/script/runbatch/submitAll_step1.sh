4ac573bdf7ecb780006fcf1f09a2d6b74734111c
fanxia <fanxia08@gmail.com>
2016-11-03 20:25:39 +0100

    updata

---
+++ susy/script/runbatch/submitAll_step1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#          inputpath, outputname, events/jobs
+#./submitMC_step1.py root://eoscms//store/group/phys_smp/ggNtuples/13TeV/MC/V08_00_11_01/job_spring16_DYJetsToLL_m50_aMCatNLO.root DYLL 1000000
+
+
+
+
+
+
+
+
+./submitData_step1.py root://eoscms//store/group/phys_smp/ggNtuples/13TeV/data/V08_00_11_01/job_SingleEle_Run2016D_PRv2.root SingleEleRun2016D 2000000
