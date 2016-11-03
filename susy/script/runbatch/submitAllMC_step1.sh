d04831433b1f599b4840bb4df07fadb7b181a9e9
fanxia <fanxia08@gmail.com>
2016-11-03 16:02:37 +0100

    adding jobs submit batch

---
+++ susy/script/runbatch/submitAllMC_step1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#          inputpath, outputname, events/jobs
+./submitMC_step1.py root://eoscms//store/group/phys_smp/ggNtuples/13TeV/MC/V08_00_11_01/job_spring16_DYJetsToLL_m50_aMCatNLO.root DYLL 1000000
